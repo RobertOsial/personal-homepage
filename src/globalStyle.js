6736100ca06c32f925d2175107554e18d491d513
Robert Osial <rob.osial@gmail.com>
2022-05-21 08:15:38 +0100

    added Normalize, GlobalStyle and ThemeProvider

---
+++ src/globalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+ html {
+    box-sizing: border-box;
+    font-family: 'Inter', sans-serif;
+   }
+
+  *, ::after, ::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+  }
+`;
