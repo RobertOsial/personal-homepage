52846131f7e122ad427b753f9c25959136b37b7a
Robert Osial <rob.osial@gmail.com>
2022-05-28 12:21:06 +0100

    added colors to theme

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,9 @@ export const theme = {
   color: {
     white: "#FFFFFF",
     backgroundLight: "#FBFBFE",
+    lightGrey: "#E5E5E5",
     darkGrey: "#6E7E91",
     black: "#252525",
+    blue: "#0366D6",
   },
 }
