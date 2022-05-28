53ea5802795202b8f4e3f089d0094216916eba80
Robert Osial <rob.osial@gmail.com>
2022-05-28 12:19:12 +0100

    StyledList and ListItem added

---
+++ src/common/List/styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const StyledList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  color: ${({ theme }) => theme.color.darkGrey};
+  padding: 0;
+  list-style: none;
+  line-height:  1.4;
+  font-weight: 400;
+  font-size: 18px;
+`;
+
+export const ListItem = styled.li`
+  position: relative;
+  padding-left: 25px;
+
+  &:before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%); 
+    width: 9px;
+    height: 9px;
+    background-color: ${({ theme }) => theme.color.blue};
+    border-radius: 50%;
+  }
+`;
