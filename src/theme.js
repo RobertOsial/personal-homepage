b368b2ba06a36a92deed7f788b7a3a78e075d971
RobertOsial <rob.osial@gmail.com>
2022-06-01 07:43:35 +0100

    amended color names in theme and added boxShadow

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,15 @@
 export const theme = {
   color: {
     white: "#FFFFFF",
-    backgroundLight: "#FBFBFE",
-    lightGrey: "#E5E5E5",
-    darkGrey: "#6E7E91",
-    black: "#252525",
-    blue: "#0366D6",
+    whiteLilac: "#FBFBFE",
+    mercury: "#E5E5E5",
+    ironTransparent: "rgba(209, 213, 218, 0.3)",
+    slateGray: "#6E7E91",
+    mineShaft: "#252525",
+    scienceBlue: "#0366D6",
+  },
+
+  common: {
+    boxShadow: "0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03)",
   },
 }
