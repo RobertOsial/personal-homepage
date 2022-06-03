46345335ce4d2b23a7d89fb2112b5a1c2cb0e4d8
RobertOsial <rob.osial@gmail.com>
2022-06-03 15:30:25 +0100

    changed div to section

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.section`
   background-color: ${({ theme }) => theme.color.white};
   box-shadow: ${({ theme }) => theme.common.boxShadow};
   border-radius: 4px;
