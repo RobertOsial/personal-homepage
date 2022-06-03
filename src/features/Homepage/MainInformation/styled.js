70d75643fb360f18d7a2feeae59de0f23a649423
RobertOsial <rob.osial@gmail.com>
2022-06-03 15:32:55 +0100

    added resposible width to ProfileImage

--- src/features/Homepage/MainInformation/styled.js
+++ src/features/Homepage/MainInformation/styled.js
@@ -1,44 +1,45 @@
 import styled from "styled-components";
-import { ReactComponent as EnvelopeIcon } from "./images/envelope.svg";
+import { ReactComponent as EnvelopeIcon } from "../images/envelope.svg";
 
 export const Wrapper = styled.header`
   display: grid;
   grid-template-columns: auto 1fr;
   grid-gap: 66px;
   align-items: center;
+  margin-bottom: 63px;
 `;
 
 export const ProfileImage = styled.img`
-  padding: 0 24px;
+  width: 30vw;
+  max-width: 398px;
+  border-radius: 50%;
 `;
 
 export const Content = styled.div`
 `;
 
 export const Introduction = styled.p`
-color: ${({ theme }) => theme.color.darkGrey};
 font-size: 12px;
 font-weight: 700;
 margin: 0 0 12px;
 `;
 
 export const Title = styled.h1`
-  color: ${({ theme }) => theme.color.black};
+  color: ${({ theme }) => theme.color.mineShaft};
   font-weight: 900;
   font-size: 38px;
   margin: 0 0 35px;
 `;
 
 export const AboutMe = styled.p`
-  color: ${({ theme }) => theme.color.darkGrey};
   font-weight: 400;
   font-size: 20px;
   margin: 0 0 32px;
 `;
 
 export const Button = styled.button`
-  color: ${({theme}) => theme.color.white};
-  background-color: ${({theme}) => theme.color.blue};
+  color: ${({ theme }) => theme.color.white};
+  background-color: ${({ theme }) => theme.color.scienceBlue};
   padding: 12px 16px;
   border-radius: 4px;
   font-weight: 600;
