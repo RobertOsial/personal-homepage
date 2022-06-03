63e5e98e45f41e8c14a3dc982d58948c3bc75e1c
RobertOsial <rob.osial@gmail.com>
2022-06-03 15:30:58 +0100

    added List Component

--- src/common/List/index.js
+++ src/common/List/index.js
@@ -1,12 +1,10 @@
 import { skills } from "../../features/Homepage/Skills/skills";
 import { ListItem, StyledList } from "./styled";
 
-export const List = () => {
+export const List = ({ listContent }) => {
   return (
     <StyledList>
-      {skills.map((skill, index) => (
-        <ListItem key={index}>{skill.content}</ListItem>
-      ))}
+      {listContent}
     </StyledList>
   );
 };
