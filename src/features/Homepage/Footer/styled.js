dfd76d0c87684debffaac6c64834fb75b0217a1e
RobertOsial <rob.osial@gmail.com>
2022-07-09 11:09:15 +0100

    added styles for Footer

---
+++ src/features/Homepage/Footer/styled.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.footer`
+  color: ${({ theme }) => theme.color.mineShaft};
+`;
+
+export const Address = styled.address`
+
+`;
+
+export const LetsTalk = styled.h2`
+  color: ${({ theme }) => theme.color.slateGray};
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 1.3;
+  margin: 0 0 24px;
+`;
+
+export const Email = styled.div`
+  font-weight: 900;
+  font-size: 32px;
+  margin-bottom: 24px;
+
+`;
+
+export const Paragraph = styled.p`
+  margin: 0 0 56px;
+  max-width: 670px;
+`;
+
