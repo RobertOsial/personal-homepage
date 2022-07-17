db0e4109c66f6b5491bf580e0fab3dfc574907ee
RobertOsial <rob.osial@gmail.com>
2022-07-17 12:10:11 +0100

    added themeSwitch to theme

--- src/theme.js
+++ src/theme.js
@@ -49,6 +49,10 @@ export const themeLight = {
       borderHover: colorNames.tropicalBlue,
       header: colorNames.scienceBlue,
     },
+    themeSwitch: {
+      background: colorNames.mercury,
+      icon: colorNames.white,
+    }
   },
 };
 
@@ -73,22 +77,9 @@ export const themeDark = {
       borderHover: colorNames.blumine,
       header: colorNames.white,
     },
+    themeSwitch: {
+      background: colorNames.doveGray,
+      icon: colorNames.mineShaft,
+    }
   },
 };
-
-
-export const theme = {
-  color: {
-    white: "#FFFFFF",
-    whiteLilac: "#FBFBFE",
-    mercury: "#E5E5E5",
-    ironTransparent: "rgba(209, 213, 218, 0.3)",
-    slateGray: "#6E7E91",
-    mineShaft: "#252525",
-    scienceBlue: "#0366D6",
-  },
-
-  common: {
-    boxShadow: "0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03)",
-  },
-}
