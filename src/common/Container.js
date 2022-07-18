c857920c22b4f9b4390c14de9c801e534009dc3e
RobertOsial <rob.osial@gmail.com>
2022-07-18 16:29:01 +0100

    adjusted Container margin on mediaMax breakpoint

--- src/common/Container.js
+++ src/common/Container.js
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   max-width: 1216px;
-  margin: 115px auto;
+  margin: 115px auto 109px;
   padding: 0 24px;
+
+  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+    margin: 32px auto 31px;
+  }
 `;
