1d17017397167429a5549823ededf134b9dccc39
RobertOsial <rob.osial@gmail.com>
2022-07-22 09:14:19 +0100

    added sama middleware to store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 import themeReducer from "./common/ThemeSwitch/themeSlice";
+import homepageReducer from "./features/Homepage/hompageSlice";
+import { homepageSaga } from "./features/Homepage/homepageSaga";
 
-export default configureStore({
+const sagaMiddleWare = createSagaMiddleware();
+
+const store = configureStore({
   reducer: {
     theme: themeReducer,
+    homepage: homepageReducer,
   },
+  middleware: [sagaMiddleWare],
 });
+
+sagaMiddleWare.run(homepageSaga);
+
+export default store;
