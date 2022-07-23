66c48a4a17a1984ebc61d6bf7db4992522f1958d
RobertOsial <rob.osial@gmail.com>
2022-07-23 10:24:44 +0100

    added styles to Footer Component

--- src/features/Homepage/Footer/styled.js
+++ src/features/Homepage/Footer/styled.js
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.footer`
-  color: ${({ theme }) => theme.colors.textPrimary};
+  margin-bottom: 120px;
+
+  @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    margin-bottom: 31px;
+  }
 `;
 
 export const Address = styled.address`
-
+  color: ${({ theme }) => theme.colors.textPrimary};
+  font-style: unset;
+  margin-bottom: 56px;
 `;
 
 export const LetsTalk = styled.h2`
@@ -14,17 +20,41 @@ export const LetsTalk = styled.h2`
   font-size: 12px;
   line-height: 1.3;
   margin: 0 0 24px;
+  letter-spacing: initial;
 `;
 
-export const Email = styled.div`
+export const EmailWrapper = styled.div`
+  margin: 24px 0;
+
+  @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    margin: 12px 0;
+  }
+`;
+
+export const EmailLink = styled.a`
   font-weight: 900;
   font-size: 32px;
-  margin-bottom: 24px;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.textPrimary};
+  transition: color 0.3s;
+  
+  &:hover {
+    color: ${({ theme }) => theme.colors.primary};
+  }
 
+  @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 18px;
+  }
 `;
 
 export const Paragraph = styled.p`
-  margin: 0 0 56px;
+  margin: 0;
   max-width: 670px;
+  line-height: 1.4;
+  font-size: 18px;
+
+  @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 14px;
+  }
 `;
 
