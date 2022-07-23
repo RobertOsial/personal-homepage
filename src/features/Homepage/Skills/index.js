5c887bf8eccf5fa24c9e8facc793cb9f16c59b6c
RobertOsial <rob.osial@gmail.com>
2022-07-23 10:31:55 +0100

    added Skills Component

--- src/features/Homepage/Skills/index.js
+++ src/features/Homepage/Skills/index.js
@@ -1,11 +1,18 @@
 import { List } from "../../../common/List";
+import { ListItem } from "../../../common/List/styled";
 import { Section } from "../../../common/Section";
+import { skills } from "./skills";
 
 export const Skills = () => {
+  const listContent = skills.map((skill, index) => (
+    <ListItem key={index}>{skill.content}</ListItem>
+  ));
+
   return (
     <Section
       title="My skillset includes 🛠️"
-      body={<List />}
+      body={<List listContent={listContent} />}
     />
   );
 }
+
