a852b925cc2ff0ce570a9963a9defe5a6ad26d1b
RobertOsial <rob.osial@gmail.com>
2022-07-23 10:30:58 +0100

    added media queries - fixed responsiveness

--- src/features/Homepage/MainInformation/styled.js
+++ src/features/Homepage/MainInformation/styled.js
@@ -1,54 +1,76 @@
 import styled from "styled-components";
-import { ReactComponent as EnvelopeIcon } from "../images/envelope.svg";
+import { ReactComponent as EnvelopeIcon } from "./envelope.svg";
 
 export const Wrapper = styled.header`
+  margin-top: -30px;
   display: grid;
   grid-template-columns: auto 1fr;
   grid-gap: 66px;
   align-items: center;
   margin-bottom: 63px;
+
+  @media(max-width: ${({theme}) => theme.breakpoints.tabletVerticalMax}px) {
+    grid-gap: 32px;
+  }
+
+  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+    grid-template-columns: 1fr;
+    grid-gap: 12px;
+  }
 `;
 
 export const ProfileImage = styled.img`
   width: 30vw;
   max-width: 398px;
   border-radius: 50%;
+
+  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+    width: 132px;
+  }
 `;
 
-export const Content = styled.div`
-`;
-
-export const Introduction = styled.p`
+export const ThisIs = styled.p`
 font-size: 12px;
 font-weight: 700;
 margin: 0 0 12px;
+letter-spacing: initial;
 `;
 
-export const Title = styled.h1`
+export const Name = styled.h1`
   color: ${({ theme }) => theme.colors.textPrimary};
   font-weight: 900;
   font-size: 38px;
   margin: 0 0 35px;
+
+  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+    font-size: 22px;
+  }
 `;
 
 export const AboutMe = styled.p`
   font-weight: 400;
   font-size: 20px;
   margin: 0 0 32px;
+  max-width: 650px;
+  line-height: 1.4;
+
+  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px) {
+    font-size: 16px;
+    max-width: 570px;
+  }
 `;
 
 export const ButtonLink = styled.a`
   color: ${({ theme }) => theme.colors.buttonLink.text};
   background-color: ${({ theme }) => theme.colors.primary};
-  padding: 12px 16px;
-  border-radius: ${({ theme }) => theme.borderRadiusSmall};
-  font-weight: 600;
-  display: flex;
-  align-items: center;
   border: 1px solid ${({ theme }) => theme.colors.buttonLink.border};
+  border-radius: ${({ theme }) => theme.borderRadiusSmall};
+  padding: 12px 16px;
+  font-weight: 600;
+  display: inline-flex;
+  align-items: center;
   gap: 16px;
   text-decoration: none;
-  width: 150px;
   transition: box-shadow 0.3s;
 
   &:hover {
